49613eda75d6cd914c92dc5da397a8c201256d65
condezero <condezero.monalisa@gmail.com>
2021-01-05 18:07:53 +0100

    first commit

---
+++ src/database/index.js
@@ -0,0 +1,15 @@
+import affectation from "./affectation";
+import cultural_origins from "./cultural_origins";
+import dress from "./dress";
+import hairstyle from "./hairstyle";
+import personality from "./personality";
+import roles from "./roles";
+
+export default {
+    affectation, 
+    cultural_origins, 
+    dress, 
+    hairstyle, 
+    personality,
+    roles
+};
